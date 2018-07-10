13f63d3ef09fdc28821c9d2fb0b3f96641089a61
Gabriele Coppola <gabriele.coppola@giuneco.it>
2018-07-10 18:24:35 +0200

    wip on login ios

---
+++ GiunecoTeam.Ios2/LoginViewController.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace GiunecoTeam.Ios2
+{
+    public partial class LoginViewController : UIViewController
+    {
+        public LoginViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ GiunecoTeam.Ios2/RootViewController.cs
@@ -0,0 +1,29 @@
+﻿using Foundation;
+using System;
+using GiunecoTeam.Domain;
+using GiunecoTeam.Domain.Resources;
+using GiunecoTeam.Domain.Resources.Impl;
+using UIKit;
+
+namespace GiunecoTeam.Ios2
+{
+    public partial class RootViewController : UITabBarController
+    {
+        private IUserResource _userResource;
+
+        public RootViewController (IntPtr handle) : base (handle)
+        {
+            _userResource = new UserResource();
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            var loginViewController = this.Storyboard.InstantiateViewController("Login") as LoginViewController;
+            if (loginViewController == null)
+                return;
+
+            this.PresentModalViewController(loginViewController, true);
+            base.ViewDidAppear(animated);
+        }
+    }
+}
